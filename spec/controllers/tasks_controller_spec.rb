0062e639bf85bfefc7302d35d041cd83acfd2e77
Steven C <stevencch99@gmail.com>
2019-07-21 21:31:27 +0800

    Setup Travis successful, add test cases of task creation.

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-describe '任務新增功能' do
-  it '建立完之後要檢查資料庫是否確實有建立' do
-    u1 = User.create(name: 'Admin', email: 'admin@email.com', tel: '0911222333')
-    Task.create(title: 'Test', user: u1)
-    expect(Task.count).to eq 1
+  let(:user1) { User.create(name: 'Admin', email: 'admin@email.com', tel: '0911222333') }
+  let(:task1) { Task.create(title: 'TestTask1', description: 'Description of task1', state: 0, priority: 0, start_time: '2019-07-21 20:11:02', end_time: '2019-07-21 20:11:22', user: user1) }
+  describe '任務新增功能' do
+    it '建立完之後要檢查資料庫是否確實有建立' do
+      create_task = Task.new(title: 'TestTask1', user: user1)
+      expect(create_task.save!).to be true
+    end
+    it '確認任務數量' do
+      2.times { Task.create(title: 'TestTask2', user: user1) }
+      expect(Task.count).to eq 2
+    end
+    it '確認 任務的標題、內容、時間、優先度等等都要和建立的一樣' do
+      expect(task1.title).to eq 'TestTask1'
+      expect(task1.description).to eq 'Description of task1'
+      expect(task1.priority).to eq 'high'
+      expect(task1.state).to eq 'pending'
+      expect(task1.user).to be user1
+      expect(task1.start_time).to eq '2019-07-21 20:11:02'.in_time_zone('Taipei')
+      expect(task1.end_time).to eq '2019-07-21 20:11:22'.in_time_zone('Taipei')
+    end
   end
-  it '還要確認任務數量'
-  it '確認 任務的標題、內容、時間、優先度等等都要和建立的一樣' 
-end
 end
