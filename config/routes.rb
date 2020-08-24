65a1bf525cd2efbd3ca5f4c176c1c4ab856bc63a
Steven Chang <stevenchang@osp.com.tw>
2020-08-24 17:43:26 +0800

    Add cancel editing/create task funciton to "Cancel Button"

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/bs', as: 'rails_admin'
   # 首頁
   root 'tasks#index'
+  get 'cancel_task', to: 'tasks#cancel'
   resources :tasks
 end
